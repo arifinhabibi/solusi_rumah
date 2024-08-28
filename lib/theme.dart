f87b5fd0c3e4980144b92be0920c120bb989c934
Muhammad Arifin Habibi <arifinhabibi76@gmail.com>
2024-08-28 10:51:54 +0700

    initial commit

---
+++ lib/theme.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+// primary colors
+const Color primary100 = Color(0xffFFF5E5);
+const Color primary200 = Color(0xffFFE2B7);
+const Color primary300 = Color(0xffFFD08A);
+const Color primary400 = Color(0xffFFBE5C);
+const Color primary500 = Color(0xffFFAB2E);
+const Color primary600 = Color(0xffFF9900); // main
+const Color primary700 = Color(0xffD68100);
+const Color primary800 = Color(0xffAD6800);
+const Color primary900 = Color(0xff855000);
+const Color primary1000 = Color(0xff5C3700);
+
+// neutral colors
+const Color neutral100 = Color(0xFFF5F5F5);
+const Color neutral200 = Color(0xFFE1E1E1);
+const Color neutral300 = Color(0xFFCCCCCC);
+const Color neutral400 = Color(0xFFB8B8B8);
+const Color neutral500 = Color(0xFFA3A3A3);
+const Color neutral600 = Color(0xFF8F8F8F);
+const Color neutral700 = Color(0xFF7B7B7B);
+const Color neutral800 = Color(0xFF666666);
+const Color neutral900 = Color(0xFF505152);
+const Color neutral1000 = Color(0xFF3B3C3D); // main
+
+// success colors
+const Color success100 = Color(0xFFEEFFF5);
+const Color success200 = Color(0xFFC9FFE0);
+const Color success300 = Color(0xFFA3FFCA);
+const Color success400 = Color(0xFF7EFFB4);
+const Color success500 = Color(0xFF54F297);
+const Color success600 = Color(0xFF3BD07A);
+const Color success700 = Color(0xFF27AE60);
+const Color success800 = Color(0xFF178C48);
+const Color success900 = Color(0xFF0B6A33);
+const Color success1000 = Color(0xFF034820); // main
+
+// theme color
+const Color background = Color(0xFFF2F6FC);
+const Color surface = Color(0xFFFFFFFF);
+
+// font color
+const Color fontHeading = neutral1000;
+const Color fontDescription = neutral400;
+const Color fontLink = primary600;
+
+// border color
+const Color borderDefault = neutral400;
+const Color borderFocused = primary600;
+const Color borderFilled = neutral400;
+
+// icon color
+const Color iconDefault = neutral400;
+const Color iconActive = primary600;
+
+//  scale
+ const double scale4XS = 1.0;
+ const double scale3XS = 2.0;
+ const double scale2XS = 4.0;
+ const double scaleXS = 6.0;
+ const double scaleSM = 8.0;
+ const double scaleMD = 12.0;
+ const double scaleLG = 16.0;
+ const double scaleXL = 20.0;
+ const double scale2XL = 24.0;
+ const double scale3XL = 32.0;
+ const double scale4XL = 40.0;
+
+//  radius
+ const double radius4XS = scale4XS;
+ const double radius3XS = scale3XS;
+ const double radius2XS = scale2XS;
+ const double radiusXS = scaleXS;
+ const double radiusSM = scaleSM;
+ const double radiusMD = scaleMD;
+ const double radiusLG = scaleLG;
+ const double radiusXL = scaleXL;
+ const double radius2XL = scale2XL;
+ const double radius3XL = scale3XL;
+ const double radius4XL = scale4XL;
+
+//  spacing
+ const double spacing4XS = scale4XS;
+ const double spacing3XS = scale3XS;
+ const double spacing2XS = scale2XS;
+ const double spacingXS = scaleXS;
+ const double spacingSM = scaleSM;
+ const double spacingMD = scaleMD;
+ const double spacingLG = scaleLG;
+ const double spacingXL = scaleXL;
+ const double spacing2XL = scale2XL;
+ const double spacing3XL = scale3XL;
+ const double spacing4XL = scale4XL;
+
+//  typhography
+const TextStyle extraLarge = TextStyle(
+  fontSize: 22.0,
+  fontFamily: 'Montserrat',
+  height: 1.5
+);
+
+const TextStyle large = TextStyle(
+  fontSize: 19.0,
+  fontFamily: 'Montserrat',
+  height: 1.5
+);
+
+const TextStyle medium = TextStyle(
+  fontSize: 16.0,
+  fontFamily: 'Montserrat',
+  height: 1.5
+);
+
+const TextStyle small = TextStyle(
+  fontSize: 13.0,
+  fontFamily: 'Montserrat',
+  height: 1.5
+);
+
+const TextStyle extraSmall = TextStyle(
+  fontSize: 11.0,
+  fontFamily: 'Montserrat',
+  height: 1.5
+);
+
+const TextStyle smallDescription = TextStyle(
+  fontSize: 9.0,
+  fontFamily: 'Montserrat',
+  height: 1.5
+);
+
